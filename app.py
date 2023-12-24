922667b2328bfb66b5dca23c9b28720ec3ef021b
unknown <brannongarrett77@gmail.com>
2023-12-24 13:26:56 +0100

    Finished the code

---
+++ app.py
@@ -0,0 +1,29 @@
+from cs50 import SQL
+from flask import Flask, render_template, request
+from helpers import random_string
+import random
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///history.db")
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        page = request.form.get("page")
+
+        try:
+            page = int(page)
+        except ValueError:
+            return render_template("index.html", random_string="Enter a valid page number!")
+        if page < 0:
+            return render_template("index.html", random_string="Type in a positive number!")
+
+        db.execute("INSERT INTO history (page) VALUES (?)", page)
+        random.seed(page)
+
+    string = random_string(1000)
+    rows = db.execute("SELECT * FROM history;")
+    return render_template("index.html", random_string=string, search_history=rows)
